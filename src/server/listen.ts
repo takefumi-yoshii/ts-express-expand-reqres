d2ae598571e01ff3ded4e11f9bde4c329a98ea4c
takefumi-yoshii <takefumi.yoshii@dena.jp>
2019-06-05 11:27:29 +0900

    remove err handle

--- src/server/listen.ts
+++ src/server/listen.ts
@@ -2,8 +2,7 @@ import Express from 'express'
 import { APP_HOST, APP_PORT } from '../constants'
 
 export default (app: Express.Application) => {
-  app.listen(APP_PORT, APP_HOST, (err: Express.Errback) => {
-    if (err) throw err
+  app.listen(APP_PORT, APP_HOST, () => {
     console.log(`Running on http://${APP_HOST}:${APP_PORT}`)
   })
 }
